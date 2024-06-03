34f8e0458c959ffabcc29e5658a791bc5e058735
Muharrem Efe Çayırbahçe <muharremefecayirbahce@gmail.com>
2024-06-03 16:39:01 +0300

    🚀Notes added

---
+++ Testing Exercise/README.md
@@ -0,0 +1,11 @@
+# Testing Exercise
+
+## Description
+
+This is a simple exercise to practice writing tests with Jest.
+
+## Instructions
+
+1. Fork and clone this repository.
+2. Run `npm install` to install dependencies.
+3. Run `npm run test` to run the tests.
---
+++ Testing Exercise/script2.js
@@ -0,0 +1,29 @@
+// const fetch = require('node-fetch');
+
+const getPeoplePromise = fetch => {
+    return fetch('http://swapi.py4e.com/api/people')
+        .then(response => response.json())
+        .then(data => {
+            return {
+                count: data.count,
+                results: data.results
+            }
+        });
+}
+
+const getPeople = async fetch => {
+    const getRequest = await fetch('http://swapi.py4e.com/api/people');
+    const data = await getRequest.json();
+    return {
+        count: data.count,
+        results: data.results
+    }
+}
+
+getPeople(fetch);
+
+module.exports = {
+    getPeople,
+    getPeoplePromise
+}
+
